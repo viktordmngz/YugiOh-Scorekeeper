c7b88aa4f3ac54259577d686bd1dcd11e0098c4f
Viktor Dominguez <95234756+viktordmngz@users.noreply.github.com>
2023-04-03 21:03:18 -0400

    Update Yugioh.py

--- Yugioh.py
+++ Yugioh.py
@@ -2,7 +2,7 @@
 Created By: Viktor Dominguez
 Tested By: Viktor Dominguez
 Date Started: 02/12/2023 (mm/dd/yyyy)
-Last Updated: 02/12/2023 (mm/dd/yyyy)
+Last Updated: 04/03/2023 (mm/dd/yyyy)
 Editor: Sublime Text
 
 ------------
@@ -33,39 +33,105 @@ from random import randint
 import sys
 from time import sleep
 import os
-
+#
+#
 # Function to clear the screen
 def clear():
 	os.system('cls' if os.name == 'nt' else 'clear')
-
-
+#
+#
 # Delayed Text Output Function
 def delay_readout(string):
 	for char in string:
 		sys.stdout.write(char)
 		sys.stdout.flush()
 		sleep(0.05)
-
-
+#
 class player:
-	def __init__(self, name, lifepoints=8000):
+	def __init__(self, name, lifepoints = 8000, defense = 0):
+		# Will use starting value to calculate health bars
+		self.startingPoints = lifepoints
 		self.name = name
 		self.lifepoints = lifepoints
 		# Display of health bars (=)
 		self.health = "="*20
 		# Bars will always mod to 20
 		self.bars = 20
-
+		# Defense of monsters
+		self.defense = defense
+#
+	# Player's turn function
 	def playerturn(self, player2):
-		clear()
-		delay_readout(f"It is {self.name}'s turn!")
-		print(f"\n\n\t\t{self.name}'s Life Points: {self.lifepoints}\t\t{self.health}")
-		print(f"\n\t\t{player2.name}'s Life Points: {player2.lifepoints}\t\t{player2.health}\n\n")
-		print("*"*40)
-		delay_readout(f"\n\n{self.name} attacks! Enter your attack power: ")
-		attack = int(input())
-	
-
+		# Main status display: players, health, and LP remaining
+		delay_readout(f"\n\nIt is {self.name}'s turn!")
+		print(f"\n\n{self.name}'s Life Points: {self.lifepoints}\t\t{self.health}")
+		print(f"\n\n{player2.name}'s Life Points: {player2.lifepoints}\t\t{player2.health}\n\n")
+		print("*"*100)
+		sleep(2.0)
+#
+		while True:
+			try:
+				delay_readout(f"\n\nPlease enter in {player2.name}'s defense points: ")
+				player2.defense = int(input())
+				break
+			except ValueError:
+				delay_readout(f"\n\nYou have entered an invalid value.")
+				sleep(1.5)
+				continue
+			delay_readout(f"\n\n{self.name} attacks!")
+		while True:
+			sleep(1.5)
+			try:
+				delay_readout(f"\n\nEnter your attack's power: ")
+				attack = int(input())
+				break
+			except ValueError:
+				delay_readout(f"\n\nPlease enter in a valid number")
+		total = attack - player2.defense
+		if total < 0:
+			delay_readout(f"\n\n{self.name} failed to inflict any damage to {player2.name}'s life points.")
+			return
+		player2.lifepoints -= total
+		# Want the percentage of lifepoints left * 20 bars 
+		player2.bars = (1-player2.lifepoints/player2.startingPoints)* 20
+		player2.health = "="*int(player2.bars)
+		delay_readout(f"\n\n{self.name} attacked for {total} damage.")
 
 if __name__ == '__main__':
-	pass
+	delay_readout("\nEnter the starting lifepoints or hit ENTER for the default: ")
+	try:
+		startValue = int(input())
+	except ValueError:
+		delay_readout("\n\nThe default value will be used.")
+		# Default value = 8000
+		startValue = 8000
+	delay_readout("\n\nPlease enter Player 1's Name: ")
+	name1 = input()
+	player1 = player(name1, startValue)
+	delay_readout("\n\nPlease enter Player 2's Name: ")
+	name2 = input()		
+	player2 = player(name2, startValue)
+#
+	def game(player1, player2):
+		clear()
+		delay_readout("\n\nIt's time to d-d-d-d-d-d-duel!")
+		while player1.lifepoints and player2.lifepoints > 0:
+			player1.playerturn(player2)
+			if player1.lifepoints <= 0:
+				delay_readout(f"\n\n{player2.name} wins!\n\n")
+				sleep(1.5)
+				break
+			elif player2.lifepoints <= 0:
+				delay_readout(f"\n\n{player1.name} wins!\n\n")
+				sleep(1.5)
+				break
+			player2.playerturn(player1)
+			if player1.lifepoints <= 0:
+				delay_readout(f"\n\n{player2.name} wins!\n\n")
+				sleep(1.5)
+				break
+			elif player2.lifepoints <= 0:
+				delay_readout(f"\n\n{player1.name} wins!\n\n")
+				sleep(1.5)
+				break
+	game(player1, player2)
